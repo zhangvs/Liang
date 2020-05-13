07859423628010cceafa2f6bcff97fa3e1766ffc
zhangyanshan <824478152@qq.com>
2020-05-13 21:34:42 +0800

    修改沙箱测试环境，沙箱环境，需要下载沙箱钱包，不能唤起真实的支付宝付款

--- HZSoft.Application/HZSoft.Application.Web/Areas/webapp/Controllers/Shop2Controller.cs
+++ HZSoft.Application/HZSoft.Application.Web/Areas/webapp/Controllers/Shop2Controller.cs
@@ -186,7 +186,7 @@ namespace HZSoft.Application.Web.Areas.webapp.Controllers
 
                 if (ordersEntity.PayType == "alipay")
                 {
-                    Redirect("alipay");
+                    Redirect("Alipay");
                 }
 
                 H5Response root = null;
@@ -446,78 +446,7 @@ namespace HZSoft.Application.Web.Areas.webapp.Controllers
             return View();
         }
 
-
-
-
-
-
-
-
-
-
-        public ActionResult zfb()
-        {
-            IAopClient client = Utility.AliPay.AliPay.GetAlipayClient();
-            //IAopClient client = new DefaultAopClient("https://openapi.alipay.com/gateway.do", "app_id", "merchant_private_key", "json", "1.0", "RSA2", "alipay_public_key", "GBK", false);
-            AlipayTradeCreateRequest request = new AlipayTradeCreateRequest();
-            request.BizContent = "{" +
-            "\"out_trade_no\":\"20150320010101001\"," +
-            "\"seller_id\":\"2088102146225135\"," +
-            "\"total_amount\":88.88," +
-            "\"discountable_amount\":8.88," +
-            "\"subject\":\"Iphone6 16G\"," +
-            "\"body\":\"Iphone6 16G\"," +
-            "\"buyer_id\":\"2088102146225135\"," +
-            "      \"goods_detail\":[{" +
-            "        \"goods_id\":\"apple-01\"," +
-            "\"goods_name\":\"ipad\"," +
-            "\"quantity\":1," +
-            "\"price\":2000," +
-            "\"goods_category\":\"34543238\"," +
-            "\"categories_tree\":\"124868003|126232002|126252004\"," +
-            "\"body\":\"特价手机\"," +
-            "\"show_url\":\"http://www.alipay.com/xxx.jpg\"" +
-            "        }]," +
-            "\"product_code\":\"FACE_TO_FACE_PAYMENT\"," +
-            "\"operator_id\":\"Yx_001\"," +
-            "\"store_id\":\"NJ_001\"," +
-            "\"terminal_id\":\"NJ_T_001\"," +
-            "\"extend_params\":{" +
-            "\"sys_service_provider_id\":\"2088511833207846\"," +
-            "\"card_type\":\"S0JP0000\"" +
-            "    }," +
-            "\"timeout_express\":\"90m\"," +
-            "\"settle_info\":{" +
-            "        \"settle_detail_infos\":[{" +
-            "          \"trans_in_type\":\"cardAliasNo\"," +
-            "\"trans_in\":\"A0001\"," +
-            "\"summary_dimension\":\"A0001\"," +
-            "\"settle_entity_id\":\"2088xxxxx;ST_0001\"," +
-            "\"settle_entity_type\":\"SecondMerchant、Store\"," +
-            "\"amount\":0.1" +
-            "          }]," +
-            "\"settle_period_time\":\"7d\"" +
-            "    }," +
-            "\"logistics_detail\":{" +
-            "\"logistics_type\":\"EXPRESS\"" +
-            "    }," +
-            "\"business_params\":{" +
-            "\"campus_card\":\"0000306634\"," +
-            "\"card_type\":\"T0HK0000\"," +
-            "\"actual_order_time\":\"2019-05-14 09:18:55\"" +
-            "    }," +
-            "\"receiver_address_info\":{" +
-            "\"name\":\"张三\"," +
-            "\"address\":\"上海市浦东新区陆家嘴银城中路501号\"," +
-            "\"mobile\":\"13120180615\"," +
-            "\"zip\":\"200120\"," +
-            "\"division_code\":\"310115\"" +
-            "    }" +
-            "  }";
-            AlipayTradeCreateResponse response = client.Execute(request);
-            Console.WriteLine(response.Body);
-            return null;
-        }
+        
         /// <summary>
         /// 订单编号
         /// </summary>
